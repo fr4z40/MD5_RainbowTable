b676b02b7f22749514cb5f038a1ddce799f09218
Eduardo Frazão <eduardo_frazao@live.com>
2015-03-13 03:22:47 -0300

    Update md5_rainbowtable.py
    some edits

--- md5_rainbowtable.py
+++ md5_rainbowtable.py
@@ -2,12 +2,9 @@
 # -*- coding: utf-8 -*-
 # Author: Eduardo Frazão
 # 16/07/2014
+
 from hashlib import md5
 from argparse import ArgumentParser
-
-def usage():
-    print('\t./script.py PasswordsList RainbowTable')
-    quit()
 
 def writer(x,y):
     with open(y, 'a') as outp:
@@ -15,8 +12,8 @@ def writer(x,y):
         outp.close()
 
 parser = ArgumentParser()
-parser.add_argument('input', help='save the output', type=str)
-parser.add_argument('-o', '--output', help='save the output', type=str)
+parser.add_argument('input', help='example: md5_rainbowtable.py /path/wordlist.ext', type=str)
+parser.add_argument('-o', '--output', help='save in a file. --output=/path/path/path/file.ext', type=str)
 args = parser.parse_args()
 
 try:
@@ -30,13 +27,19 @@ except Exception as erro:
 pass_list = list(set(pass_list))
 pass_list.sort()
 
+
+print('|%sHash%s|  String\n' % ((' '*15),(' '*15)))
+
 for pswd in pass_list:
     pswd = (((pswd.replace('\r', '')).replace('\n', '')).replace('\t', ''))
     outp = ('%s:%s\n' % (((md5(bytes(pswd, 'utf8'))).hexdigest()), pswd))
+    out = ((outp.strip('\n')).split(':'))
 
     if args.output:
         try:
             writer(outp, args.output)
         except Exception as error:
             print('\n\n%s\n\n' % str(error))
-    print(outp.strip('\n'))
+
+    print('| %s |  %s' % (out[0], out[1]))
+    
